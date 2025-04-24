bcc1ba7dc313e32d111334dbc713eea69c983c24
hito16 <16582909+hito16@users.noreply.github.com>
2025-04-23 18:51:46 -0700

    cleanup 2-directory, shrink screenshots

--- examples/2-directory/directories.c
+++ examples/2-directory/directories.c
@@ -1,87 +1,85 @@
 /*
 Example of WHBLogConsoleInit & WHBLogPrint from
 ProgrammingOnTheU ButtonTester
-WHBLogConsoleDraw must be called to draw to the screen.  I overdid it to address CEMU
-refresh issues.
+WHBLogConsoleDraw must be called to draw to the screen.  I overdid it to address
+CEMU refresh issues.
 
  About directories
- 
+
  /vol/external01 is the CWD of a running app
  On the WiiU, /vol/external01
       is the root of the SD card, and parent directory visibilty is restricted.
  In CEMU,  /vol/external01
      is just part of CEMU virtual storage. You can cd into parent and explore.
-     CEMU supports virtual SD  which is a folder on the host. Once you configure CEMU,
-     the virtual CD folder mapped to /vol/storage_mlc01 
+     CEMU supports virtual SD  which is a folder on the host. Once you configure
+CEMU, the virtual CD folder mapped to /vol/storage_mlc01
  */
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <dirent.h>
-
 
 #include <coreinit/thread.h>
-#include <whb/log_console.h>
+#include <dirent.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <whb/log.h>
+#include <whb/log_console.h>
 #include <whb/proc.h>
 
-void do_getcwd()
-{
-    char buffer[1024];
-    if (getcwd(buffer, 1024) == NULL)
-    {
-        WHBLogPrint(" Error: getcwd() failed");
-    }
-    else
-    {
-        WHBLogPrintf("== do_getcwd() %s", buffer);
-    }
+/* Force a draw immediately after WHBLogPrintf
+   Nothing will be written to the screen without WHBLogConsoleDraw
+*/
+int WHBLogPrintfDraw(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    int result = WHBLogPrintf(format, args);
+    va_end(args);
     WHBLogConsoleDraw();
+    return result;
 }
 
-void do_readdir(char *targetdir)
-{
+void do_getcwd() {
+    char buffer[1024];
+    if (getcwd(buffer, 1024) == NULL) {
+        WHBLogPrintfDraw(" Error: getcwd() failed");
+    } else {
+        WHBLogPrintfDraw("== do_getcwd() %s", buffer);
+    }
+}
+
+void do_readdir(char *targetdir) {
     DIR *dp = NULL;
     struct dirent *ep = NULL;
     int numfiles = 0;
 
     dp = opendir(targetdir);
-    if (dp != NULL)
-    {
-        WHBLogPrintf("== do_readdir %s", targetdir);
-        WHBLogConsoleDraw();
-        while (ep = readdir(dp))
-        {
+    if (dp != NULL) {
+        WHBLogPrintfDraw("== do_readdir %s", targetdir);
+        while (ep = readdir(dp)) {
             numfiles++;
-            WHBLogPrintf("%s %s", ep->d_type == DT_DIR ? "D" : " ", ep->d_name);
-            WHBLogConsoleDraw();
+            if (numfiles < 10) {
+                WHBLogPrintfDraw("%s %s", ep->d_type == DT_DIR ? "D" : " ",
+                                 ep->d_name);
+            }
         }
-        closedir(dp);
-    }
-    else
-    {
+    } else {
         WHBLogPrintf("Error: readdir() failed on %s", targetdir);
     }
-    WHBLogPrintf(" (%d) entries found", numfiles);
-    WHBLogPrint("");
-    WHBLogConsoleDraw();
+    closedir(dp);
+    WHBLogPrintf(" (%d) entries found (showing first 10)", numfiles);
+    WHBLogPrintfDraw("");
     OSSleepTicks(OSMillisecondsToTicks(3000));
 }
 
-void do_chdir(char *targetdir)
-{
-    WHBLogPrintf("== do_chdir %s", targetdir);
-    WHBLogConsoleDraw();
-    if(chdir(targetdir) == -1) {
-        WHBLogPrintf("Error: chdir() failed on %s", targetdir);
-        WHBLogConsoleDraw();
+void do_chdir(char *targetdir) {
+    WHBLogPrintfDraw("== do_chdir %s", targetdir);
+    if (chdir(targetdir) == -1) {
+        WHBLogPrintfDraw("Error: chdir() failed on %s", targetdir);
     } else {
-        do_getcwd(); 
+        do_getcwd();
     }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     WHBProcInit();
 
     /*  Use the Console backend for WHBLog - this one draws text with OSScreen
@@ -89,51 +87,52 @@ int main(int argc, char **argv)
     WHBLogConsoleInit();
 
     int times_left = 5;
-    while (WHBProcIsRunning() && times_left > 0)
-    {
+    while (WHBProcIsRunning() && times_left > 0) {
         WHBLogPrint("Do directory operations work as expected on WiiU?");
         WHBLogPrint("");
         WHBLogPrint("== Using WBLog Console mode for output (RED screen)");
-        WHBLogPrint("== ");
-        WHBLogConsoleDraw();
+        WHBLogPrintfDraw("== ");
+
         OSSleepTicks(OSMillisecondsToTicks(3000));
 
+        /*   display CWD and list directory */
         WHBLogPrint("");
-        WHBLogPrint("== on wiiu, you'll see the SD card root (/vol/external01)");
-        WHBLogPrint("== in cemu, you'll see a 'wiiu' folder");
-        WHBLogConsoleDraw();
+        WHBLogPrint("== on the wiiu, starting dir is /vol/external01");
+        WHBLogPrintfDraw("== in cemu, you'll see a 'wiiu' folder");
         OSSleepTicks(OSMillisecondsToTicks(3000));
+
         do_getcwd();
         do_readdir("./");
-        
-        WHBLogPrint("== Parent directories are only visible in CEMU, not the WIIU.");
-        WHBLogConsoleDraw();
+
+        /*  attempt to CD and list the parent directory */
+        WHBLogPrintfDraw(
+            "== Parent directories are only visible in CEMU, not the WIIU.");
         do_chdir("/vol");
         do_readdir("/vol");
 
+        /*  attempt to CD and list the root directory */
         do_chdir("/");
         do_readdir("/");
 
-        WHBLogPrint("");
-        WHBLogConsoleDraw();
-        
-        WHBLogPrint("== In CEMU, your SD card root will be mounted here");
+        WHBLogPrintfDraw("");
+
+        /* attempt to read the CEMU SD card mount point*/
+        WHBLogPrintfDraw("== In CEMU, your SD card root will be mounted here");
         do_readdir("/vol/storage_mlc01");
 
-        WHBLogPrint("== return to /vol/external01");
-        WHBLogConsoleDraw();
+        /* return to the WIIU homebrew SD card mount point */
+        WHBLogPrintfDraw("== return to WIIU SD card mount /vol/external01");
         do_chdir("/vol/external01");
 
         times_left--;
-        WHBLogPrintf("== done. runs remaining = %d", times_left);
-        WHBLogPrint("");
-        WHBLogConsoleDraw();
+        WHBLogPrintf("== done. Running again (attempts = %d)", times_left);
+        WHBLogPrintfDraw("");
+
         OSSleepTicks(OSMillisecondsToTicks(8000));
     }
 
     /*  If we get here, ProcUI said we should quit. */
-    WHBLogPrint("Shutting Down...");
-    WHBLogConsoleDraw();
+    WHBLogPrintfDraw("Shutting Down...");
 
     WHBLogConsoleFree();
     WHBProcShutdown();
